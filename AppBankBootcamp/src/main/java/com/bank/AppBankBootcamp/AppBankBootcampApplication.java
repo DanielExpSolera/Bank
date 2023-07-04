89902be3d680aed6d2038d1ecb8d22da03896baf
DanielExpSolera <daniel.exposito@solera.com>
2023-07-04 15:09:54 +0200

    add springboot app

---
+++ AppBankBootcamp/src/main/java/com/bank/AppBankBootcamp/AppBankBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.bank.AppBankBootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBankBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBankBootcampApplication.class, args);
+	}
+
+}
---
+++ AppBankBootcamp/src/test/java/com/bank/AppBankBootcamp/AppBankBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.AppBankBootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBankBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
